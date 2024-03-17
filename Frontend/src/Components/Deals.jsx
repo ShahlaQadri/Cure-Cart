a8d43d8157e222decbd3c5f83d288a3e57cb42f6
ShahlaQadri <qadrishaila99@gmail.com>
2024-03-17 16:26:01 +0530

    changes

--- Frontend/src/Components/Deals.jsx
+++ Frontend/src/Components/Deals.jsx
@@ -3,7 +3,7 @@ import Productcard from './Productcard'
 
 export default function Deals() {
   return (
-    <div className='my-4'>
+    <div className='my-6'>
       <span className='font-bold text-4xl'>Today's Best Deals for you!</span>
       <Productcard/>
     </div>
