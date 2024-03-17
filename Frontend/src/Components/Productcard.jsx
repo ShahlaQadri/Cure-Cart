a87e8c7bb5e98770b9965e75740dee528565dbfc
Sofi Salman <sofisalman9906@gmail.com>
2024-03-17 16:26:14 +0530

    deals section added

--- Frontend/src/Components/Productcard.jsx
+++ Frontend/src/Components/Productcard.jsx
@@ -1,50 +1,18 @@
-import React from 'react'
+import React from "react";
+import { BsHandbagFill } from "react-icons/bs";
 
 export default function Productcard() {
   return (
-    <div className='grid grid-cols-6 gap-5 py-4'>
-        <div className='bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl'>
-            <img src="pictures/div1.png" alt="" className='p-10' />
-            <p className='text-center font-serif'>HK Vitals</p>
-            <p className='text-center font-serif'>Multimineral Tablets</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2' >Add to Cart</button>
-        </div>
-
-        <div className="bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
-            <img src="pictures/div2.png" alt="" className='p-10'/>
-            <p className='text-center font-serif'>Cetaphil</p>
-            <p className='text-center font-serif'>Oily skin cleanser</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2' >Add to Cart</button>
-        </div>
-
-        <div className="bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
-            <img src="pictures/div1.png" alt="" className='p-10' />
-            <p className='text-center font-serif'>HK Vitals</p>
-            <p className='text-center font-serif'>Multimineral Tablets</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2' >Add to Cart</button>
-        </div>
-
-        <div className="bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
-            <img src="pictures/div2.png" alt="" className='p-10' />
-            <p className='text-center font-serif'>Cetaphil</p>
-            <p className='text-center font-serif'>Oily skin cleanser</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2' >Add to Cart</button>
-        </div>
-
-        <div className="bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
-            <img src="pictures/div1.png" alt="" className='p-10'/>
-            <p className='text-center font-serif'>HK Vitals</p>
-            <p className='text-center font-serif'>Multimineral Tablets</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2' >Add to Cart</button>
-        </div>
-
-        <div className="bg-zinc-200 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
-            <img src="pictures/div2.png" alt="" className='p-10' />
-            <p className='text-center font-serif'>Cetaphil</p>
-            <p className='text-center font-serif'>Oily skin cleanser</p>
-            <button className=' bg-[#A8DF8E] rounded-lg p-1 mt-2 ' >Add to Cart</button>
-        </div>
-
+    <div className="bg-zinc-100 p-4 rounded-xl flex flex-col items-center justify-center shadow-xl">
+      <img src="pictures/div1.png" alt="" className="px-10 py-5 w-44 h-52" />
+      <p className="text-center font-semibold">HK Vitals</p>
+      <p className="text-center text-zinc-700 text-sm">Multimineral Tablets</p>
+      <div className="flex px-5 mt-5 justify-between items-center w-full">
+        <span>$600</span>
+        <button className="  rounded-lg p-1 mt-2">
+          <BsHandbagFill className="text-xl" />
+        </button>
+      </div>
     </div>
-  )
+  );
 }
