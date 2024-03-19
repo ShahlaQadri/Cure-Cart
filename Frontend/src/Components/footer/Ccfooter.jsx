a5421f586fcf17a7a5f5b24172ce13c55ab68be1
Sofi Salman <sofisalman9906@gmail.com>
2024-03-19 18:06:11 +0530

    footer changed

--- Frontend/src/Components/footer/Ccfooter.jsx
+++ Frontend/src/Components/footer/Ccfooter.jsx
@@ -1,48 +1,43 @@
-import React from 'react'
+import React from "react";
 
 export default function Ccfooter() {
   return (
-    <div className='outer bg-[#A8DF8E] flex flex-col items-center justify-center mx-20 h-[470px] rounded-3xl'>
+    <div className="outer bg-[#163300] flex flex-col items-center justify-center mx-0 h-[590px] rounded-3xl">
+      <div className="inner bg-white h-[300px] w-[80%] rounded-3xl">
+        <div className="items flex items-center justify-evenly space-x-16 my-10">
+          <ul className="space-y-2">
+            <li className="pb-3 text-lg font-bold">Company</li>
+            <li className="text-sm leading-4">What's New</li>
+            <li className="text-sm leading-4">About Us</li>
+            <li className="text-sm leading-4">Contact Us</li>
+          </ul>
 
-        <div className="inner bg-white h-[250px] w-[70%] rounded-3xl">
-            <div className="items flex items-center justify-center space-x-16 my-6">
-                <ul className='space-y-2'>
-                    <li className='pb-3 font-bold'>Company</li>
-                    <li>What's New</li>
-                    <li>About Us</li>
-                    <li>Contact Us</li>
-                </ul>
+          <ul className="space-y-2">
+            <li className="pb-3 font-bold text-lg ">Community</li>
+            <li className="text-sm leading-4">CureCart for Business</li>
+            <li className="text-sm leading-4">Explore Templates</li>
+            <li className="text-sm leading-4">Charities</li>
+          </ul>
 
-                <ul className='space-y-2'>
-                    <li className='pb-3 font-bold'>Community</li>
-                    <li>CureCart for Business</li>
-                    <li>Explore Templates</li>
-                    <li>Charities</li>
-                </ul>
+          <ul className="space-y-2">
+            <li className="pb-3 font-bold text-lg ">Support</li>
+            <li className="text-sm leading-4">Getting Started</li>
+            <li className="text-sm leading-4">FAQs</li>
+            <li className="text-sm leading-4">Report a Violation</li>
+          </ul>
 
-                <ul className='space-y-2'>
-                    <li className='pb-3 font-bold'>Support</li>
-                    <li>Getting Started</li>
-                    <li>FAQs</li>
-                    <li>Report a Violation</li>
-                </ul>
-
-                <ul className='space-y-2'>
-                    <li className='pb-3 font-bold'>Trust & Legal</li>
-                    <li>Terms and Conditions</li>
-                    <li>Cookie Notice</li>
-                    <li>Trust Center</li>
-                </ul>
-            </div>
-
+          <ul className="space-y-2">
+            <li className="pb-3 font-bold text-lg ">Trust & Legal</li>
+            <li className="text-sm leading-4">Terms and Conditions</li>
+            <li className="text-sm leading-4">Cookie Notice</li>
+            <li className="text-sm leading-4">Trust Center</li>
+          </ul>
         </div>
+      </div>
 
-
-        <div className="logo bg-white h-[120px] w-[70%] mt-3 rounded-3xl text-center py-3">
-            <p className='text-[#A8DF8E] font-extrabold text-5xl'>C U R E</p>
-            <p className='text-[#A8DF8E] font-extrabold text-5xl'>C A R T</p>
-        </div>
-      
+      <div className="logo bg-[#9FE870] h-[160px] w-[80%] mt-3 rounded-3xl  flex items-center justify-center py-3">
+        <p className="text-[#163300] font-extrabold text-[7rem] ">CureCart</p>
+      </div>
     </div>
-  )
+  );
 }
