2e1c9d3896b1195e04527657260331158ac914e3
ShahlaQadri <qadrishaila99@gmail.com>
2024-03-19 16:35:39 +0530

    responsive prescription section

--- Frontend/src/Components/Prescription.jsx
+++ Frontend/src/Components/Prescription.jsx
@@ -3,44 +3,58 @@ import { BsFileEarmarkArrowUpFill } from "react-icons/bs";
 
 export default function Prescription() {
   return (
-    <div className="grid grid-cols-2 gap-14 py-2 mx-20">
-      <div className="prescription bg-[#FFEB68] h-[300px] w-[50] flex flex-col items-center justify-center rounded-2xl">
-        <div className="img-con h-[180px] w-[90%] bg-white rounded-xl mb-6 border-dashed border-2 border-zinc-500 pl-8 pt-4">
-          <h1 className="w-1/2 font-bold  text-2xl  mx-4 mt-4 mb-2">
+    <div className="grid grid-cols-1 gap-10 py-4 mx-4 md:grid md:grid-cols-2 md:gap-14 md:py-2 md:mx-20">
+      <div className="prescription bg-[#FFEB68] h-[180px] md:h-[300px] w-[50] flex flex-col items-center justify-center rounded-2xl">
+        <div className="img-con h-[130px] pt-2 md:h-[180px] w-[90%] bg-white rounded-xl md:mb-6 border-dashed border-2 border-zinc-500 pl-8 md:pt-4">
+          <h1 className="w-3/4 font-bold text-xl mb-2 md:mx-4 md:mt-4 md:mb-2 md:text-2xl">
             Upload prescription to place order
           </h1>
-          <p className="mx-4 text-xs font-semibold text-zinc-500">
+          <p className="md:mx-4 text-xs font-semibold text-zinc-500 my-2">
             Upload only .jpg .png or .pdf files <br />
             Size limit is 15 MB
           </p>
         </div>
 
         <div className="  items-center w-full px-10 gap-2 flex">
-          <button className="bg-black rounded-full  w-8 h-8 p-5 flex items-center justify-center text-sm  ">
+          <button className="bg-black rounded-full mt-2 w-5 h-5 p-4 md:w-8 md:h-8 md:p-5 flex items-center justify-center text-sm  ">
             <span>
               <BsFileEarmarkArrowUpFill className="text-white" />
             </span>
           </button>
 
-          <button className="button bg-black text-white px-5 py-3 rounded-full text-xs">
+          <button className="button bg-black text-white px-3 py-2 mt-2 md:px-5 md:py-3 rounded-full text-xs">
             Order Via Prescription
           </button>
         </div>
       </div>
 
-      <div className="discount bg-[#A0E1E1] flex flex-col items-center justify-center rounded-2xl">
-        <div className="img-con h-[180px] w-[90%] bg-white rounded-xl mb-6 border-dashed border-2 border-zinc-500">
-          <p className="w-1/2 font-bold text-2xl mx-4 my-4">
+
+
+      <div className="prescription bg-[#A0E1E1] h-[180px] md:h-[300px] w-[50] flex flex-col items-center justify-center rounded-2xl">
+        <div className="img-con h-[130px] pt-2 md:h-[180px] w-[90%] bg-white rounded-xl md:mb-6 border-dashed border-2 border-zinc-500 pl-8 md:pt-4">
+          <h1 className="w-3/4 font-bold text-xl mb-2 md:mx-4 md:mt-4 md:mb-2 md:text-2xl">
             Upload prescription to place order
+          </h1>
+          <p className="md:mx-4 text-xs font-semibold text-zinc-500 my-2">
+            Upload only .jpg .png or .pdf files <br />
+            Size limit is 15 MB
           </p>
-          <p className="mx-4">Upload only .jpg .png or .pdf files</p>
-          <p className="mx-4">Size limit is 15 MB</p>
         </div>
 
-        <div className="button bg-black text-white p-2 rounded-lg text-sm">
-          <button className="">Order Via Prescription</button>
+        <div className="  items-center w-full px-10 gap-2 flex">
+          <button className="bg-black rounded-full mt-2 w-5 h-5 p-4 md:w-8 md:h-8 md:p-5 flex items-center justify-center text-sm  ">
+            <span>
+              <BsFileEarmarkArrowUpFill className="text-white" />
+            </span>
+          </button>
+
+          <button className="button bg-black text-white px-3 py-2 mt-2 md:px-5 md:py-3 rounded-full text-xs">
+            Order Via Prescription
+          </button>
         </div>
       </div>
+
+
     </div>
   );
 }
--- Frontend/src/Components/Productcard.jsx
+++ Frontend/src/Components/Productcard.jsx
@@ -3,19 +3,19 @@ import { BsHandbagFill } from "react-icons/bs";
 
 export default function Productcard() {
   return (
-    <div className="  h-[360px] overflow-hidden  rounded-3xl flex flex-col items-center justify-center shadow-xl">
+    <div className="h-[260px] md:h-[360px] overflow-hidden rounded-3xl flex flex-col items-center justify-center shadow-xl">
       <div className="img-container  flex items-center justify-center w-full h-[68%] bg-[#F1F5F9]">
         <img src="pictures/div1.png" alt="" className="w-28  h-40" />
       </div>
 
       <div className="h-[16%] md:h-[37%] md:flex md:flex-col  md:w-full  rounded-3xl md:overflow-hidden bg-white md:-mt-4 md:items-center md:justify-center">
-        <p className=" md:text-center md:font-semibold md:mt-4">HK Vitals</p>
+        <p className="md:text-center md:font-semibold md:mt-4">HK Vitals</p>
         <p className="hidden sm:block md:visible md:text-center md:text-zinc-700 md:text-sm">
           Multimineral Tablets...
         </p>
-        <div className=" flex w-[50%] px-2 mt-1 md:px-5 md:mt-5 md:mb-5 justify-between items-center md:w-full">
-          <span className="text-sm">$600</span>
-          <button className=" w-5 h-7 mt-1 md:w-10 md:h-9 flex rounded-lg p-1 md:mt-2">
+        <div className="flex w-[50%] px-2 mt-1  md:px-5 md:mt-5 md:mb-5 justify-between items-center md:w-full">
+          <span className="text-sm mr-2">$600</span>
+          <button className="w-5 h-7 mt-1 md:w-10 md:h-9 flex rounded-lg p-1 md:mt-2">
             <BsHandbagFill className="text-sm md:text-xl " />
           </button>
         </div>
