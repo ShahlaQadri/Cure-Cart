f27076b3611fd8f3eafae52c5239ef712a4c69d2
ShahlaQadri <qadrishaila99@gmail.com>
2024-03-30 11:35:37 +0530

    changes in product details

--- Frontend/src/pages/ProductDetails.jsx
+++ Frontend/src/pages/ProductDetails.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import { GiMedicines } from "react-icons/gi";
 import { SiFsecure } from "react-icons/si";
 import { MdAssignmentReturned } from "react-icons/md";
-import { IoCheckmarkDoneCircleOutline } from "react-icons/io5";
 import { IoReturnUpBackSharp } from "react-icons/io5";
 
 export default function ProductDetails() {
   return (
+    <div>
     <div className='flex mx-10 bg-gray-50 rounded-3xl'>
       <div className='relative'>
       <button className='absolute bg-gray-50 shadow-lg p-4' >
@@ -19,8 +19,9 @@ export default function ProductDetails() {
 
 
       <div className="description m-10 p-10 w-[60%]">
-        <div className='space-y-3 border-b border-gray-600 pb-7'>
+        <div className='space-y-2 border-b border-gray-600 pb-6'>
           <p className='text-3xl'>Vicks Vaporub 25 ml</p>
+          <p className='text-gray-400 font-semibold text-xl'>Procter & Gamble Hygiene & Healthcare Ltd.</p>
           <div className='flex space-x-2'>
           <p className='px-2 py-1 text-gray-600 bg-gray-300 rounded-full'>Health Conditions</p>
           <p className='px-2 py-1 text-gray-600 bg-gray-300 rounded-full'>Cold and Fever</p>
@@ -28,10 +29,13 @@ export default function ProductDetails() {
         </div>
 
         <div className='flex gap-[10rem] mt-2'>
-          <div className='flex space-x-5  items-center'>
-          <p className='text-xl font-bold'>$22.80</p>
+          <div className='items-center'>
           <p className='text-gray-400 text-sm'>MRP <strike>$24.00</strike></p>
-          <p className='text-sm text-pink-600'>Save 5%</p>
+          <div className='flex gap-4'>
+          <p className='text-xl font-bold'>$22.80</p>
+          <p className='text-lg text-gray-500'>$3.40/ML</p>
+          </div>
+          {/* <p className='text-sm text-pink-600'>Save 5%</p> */}
           </div>
           <div className=''>
           <button className= 'rounded-lg p-3 bg-blue-400 text-white text-xl space'>ADD TO CART</button>
@@ -75,8 +79,35 @@ export default function ProductDetails() {
           
         
       </div>
-
       </div> 
     </div>
+
+    <div className="details  mx-10 bg-gray-50 rounded-3xl m-4 p-4">
+      <div className="about pb-4">
+        <p className='font-bold text-xl'>About Vicks Vaporub 25 ML</p>
+        <p className='text-gray-500 text-md'>Camphor Topical, Eucalyptus Oil Topical, and Menthol Topical are active components in Vicks Vaporub Ointment. Pain, Warts, Cold Sores, Hemorrhoids, Osteoarthritis, Gum Diseases, Plaques, Bleeding Gums, Delayed Onset Muscle Soreness, and more disorders are treated with Vicks Vaporub Ointment. </p>
+      </div>
+
+      <div className="uses pb-4">
+        <p className='font-bold text-xl'>Uses of Vicks Vaporub 25 ML</p>
+        <p className='text-gray-500 text-md'>Vicks Vapourab is used in treatment of Pain Warts Cold sores Hemorrhoids Osteoarthritis Gum diseases Plaques Bleeding gums Delayed onset muscle soreness</p>
+      </div>
+
+      <div className="directions">
+        <p className='font-bold text-xl'>Directions for Use of Vicks Vaporub 25 ML</p>
+        <p className='text-gray-500 text-md'>Follow the instructions written on the packaging. To treat a cough, rub Vicks VapoRub on your chest—never under your nostrils. To treat muscle aches, rub Vicks VapoRub directly over the muscle. Vicks VapoRub should never be used internally or in a manner not directed. Do not use it on broken skin or burns.</p>
+
+        <div className="expiry flex pt-4 ">
+          <p className='font-bold text-xl'>Expires on: </p>
+          <p className='font-bold text-xl pl-1'>Oct-26</p>
+        </div>
+      </div>
+
+    </div>
+
+    </div>
+
+    
+    
   )
 }
