3ac66d1a66f7399e6f1fc18a76f017de9e3f0891
Sofi Salman <sofisalman9906@gmail.com>
2024-04-12 14:20:07 +0530

    delete user in backend

--- Backend/routes/User.js
+++ Backend/routes/User.js
@@ -1,15 +1,17 @@
 import express from  'express';
-import {getAllUsers, logOut, login, myProfie, register} from "../controllers/User.js"
-import { IsAuthenticated } from "../middlewares/isAuthenticated.js";
+import { deleteUser, getAllUsers, logOut, login, myProfie, register} from "../controllers/User.js"
+import { IsAuthenticated, adminOnly } from "../middlewares/isAuthenticated.js";
 const router = express.Router();
 
 
 // user routes
 router.post("/new", register);
 router.post("/login",login )
-router.get("/allusers",getAllUsers)
+router.get("/allusers",adminOnly,getAllUsers)
 router.get("/myprofile", IsAuthenticated,myProfie);
 router.get("/logout", IsAuthenticated, logOut);
+
+router.delete("/:id",adminOnly,deleteUser)
 
 
 export default  router;
