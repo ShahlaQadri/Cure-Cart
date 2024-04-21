d89f75783fce096f1da74e775bf0ad564daa9c9e
Sofi Salman <sofisalman9906@gmail.com>
2024-04-21 15:13:48 +0530

    few changes in home page

--- Frontend/src/Components/Productcard.jsx
+++ Frontend/src/Components/Productcard.jsx
@@ -4,7 +4,10 @@ import { Link } from "react-router-dom";
 
 export default function Productcard({ img, name, desc }) {
   return (
-    <div className="h-[230px] flex-shrink-0 w-[160px] md:w-[250px] bg-[#F1F5F9] md:h-[340px] overflow-hidden rounded-3xl flex flex-col items-center justify-center shadow-sm hover:shadow-md">
+    <div className=" relative h-[230px] flex-shrink-0 w-[160px] md:w-[250px] bg-[#F1F5F9] md:h-[340px] overflow-hidden rounded-3xl flex flex-col items-center justify-center shadow-sm hover:shadow-md">
+      {/* <span className=" absolute top-6 left-0 inline-flex items-center rounded-r-full  bg-[#E3F6EE]  px-5 py-1 text-xs font-semibold text-green-400  ring-1 ring-inset ring-green-400">
+        5% off 
+      </span> */}
       <div className="img-container  flex items-center justify-center w-full h-[68%] bg-[#e7f4f7]">
         <Link to="/product">
           <img src={img} alt="" className="h-24 md:h-36" />
@@ -19,8 +22,8 @@ export default function Productcard({ img, name, desc }) {
           it is best product
         </p>
         <div className="flex text-center px-2 mt-2 md:px-5 md:mt-5 md:mb-5 justify-between items-center w-full ">
-          <span className="line-through text-[8px] text-red-700">$600</span>
-          <span className="text-[10px] text-black leading-tighter mr-16 md:mr-16">
+          <span className="line-through text-[8px] md:text-[10px] text-red-700">$600</span>
+          <span className="text-[10px] md:text-[14px] md:mr-20 text-black leading-tighter mr-16 ">
             $560
           </span>
 
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+<link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" />
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
