d794187d1c2482e3e2e88587dad2f979cdfae7c2
sofisalmanarif <sofisalman9906@gmail.com>
2024-06-15 23:45:34 +0530

    state persisted in store

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -3,13 +3,16 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import {Provider} from "react-redux"
-import { store } from './redux/store.js'
+import { persistor, store } from './redux/store.js'
+import { PersistGate } from 'redux-persist/integration/react'
 
 {/* <link rel="shortcut icon" href="./assets/favicon.ico" type="image/x-icon" /> */}
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
       <App />
-    </Provider>
+    </PersistGate>
+  </Provider>,
   </React.StrictMode>,
 )
--- Frontend/src/redux/store.js
+++ Frontend/src/redux/store.js
@@ -1,24 +1,56 @@
-import { configureStore } from '@reduxjs/toolkit'
-import { userApi } from './api/userAPI'
-import userReducer from './reducers/userReducer.js'
-import { productsApi } from './api/productsAPI.js'
-import cartReducer  from './reducers/cartReducer.js'
-import { orderApi } from './api/ordersAPI.js'
-import { dashboardApi } from './api/dashboardAPI.js'
-import { presciptionApi } from './api/presciptionAPI.js'
+import { configureStore } from '@reduxjs/toolkit';
+import { persistStore, persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage'; // defaults to localStorage for web
+import { combineReducers } from 'redux';
+import { userApi } from './api/userAPI';
+import userReducer from './reducers/userReducer';
+import { productsApi } from './api/productsAPI';
+import cartReducer from './reducers/cartReducer';
+import { orderApi } from './api/ordersAPI';
+import { dashboardApi } from './api/dashboardAPI';
+import { presciptionApi } from './api/presciptionAPI';
 
-export const serverUrl ="http://localhost:3000"
-export const store  = configureStore({
-    reducer:{
-        // Add your reducers here
-        [userApi.reducerPath]:userApi.reducer,
-        [productsApi.reducerPath]:productsApi.reducer,
-        [orderApi.reducerPath]:orderApi.reducer,
-        [dashboardApi.reducerPath]:dashboardApi.reducer,
-        [presciptionApi.reducerPath]:presciptionApi.reducer,
-        userReducer:userReducer,
-        cartReducer : cartReducer,
+// Persist configuration for the cart reducer
+const cartPersistConfig = {
+  key: 'cart',
+  storage,
+  whitelist: ['cartItems', 'shippingInfo'], // Only persist these keys from cartReducer
+};
 
-    },
-    middleware:(defmid) => [...defmid(),userApi.middleware,productsApi.middleware,orderApi.middleware,dashboardApi.middleware,presciptionApi.middleware]
-})
+// Persist configuration for the user reducer
+const userPersistConfig = {
+  key: 'user',
+  storage,
+  whitelist: ['user',], 
+};
+
+// Wrap the cart and user reducers with persistReducer
+const persistedCartReducer = persistReducer(cartPersistConfig, cartReducer);
+const persistedUserReducer = persistReducer(userPersistConfig, userReducer);
+
+// Combine all your reducers
+const rootReducer = combineReducers({
+  [userApi.reducerPath]: userApi.reducer,
+  [productsApi.reducerPath]: productsApi.reducer,
+  [orderApi.reducerPath]: orderApi.reducer,
+  [dashboardApi.reducerPath]: dashboardApi.reducer,
+  [presciptionApi.reducerPath]: presciptionApi.reducer,
+  userReducer: persistedUserReducer, // Persisted user reducer
+  cartReducer: persistedCartReducer, // Persisted cart reducer
+});
+
+// Configure the store with the root reducer and middleware
+export const store = configureStore({
+  reducer: rootReducer,
+  middleware: (defMid) =>
+    defMid().concat(
+      userApi.middleware,
+      productsApi.middleware,
+      orderApi.middleware,
+      dashboardApi.middleware,
+      presciptionApi.middleware
+    ),
+});
+
+// Create the persistor
+export const persistor = persistStore(store);
