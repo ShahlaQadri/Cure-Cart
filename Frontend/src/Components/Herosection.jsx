3a6445ae4e44e34832191c9e4908ba2ecb10f4fb
sofisalmanarif <sofisalman9906@gmail.com>
2024-06-21 16:59:43 +0530

    heading adjusted in hero section

--- Frontend/src/Components/Herosection.jsx
+++ Frontend/src/Components/Herosection.jsx
@@ -1,18 +1,21 @@
-import React from "react";
+
 
 function Herosection() {
   return (
     <div className="slider rounded-3xl flex flex-col-reverse md:flex-row text-[#3E5C75] bg-[#B0E5F2] overflow-hidden">
-      <div className="left flex flex-col justify-center items-center md:items-baseline px-2 py-4 md:px-12 md:py-12 space-y-2 md:space-y-5 md:ml-[120px]">
-         <p className="3/4 mx-4 text-center md:text-center md:text-xl ">
+      <div className="left flex flex-col justify-center items-center md:items-baseline px-2 py-4 md:px-12 md:py-12 space-y-2 md:space-y-8 md:ml-[120px]">
+         
+       <div>
+       <p className="3/4 mb-2  text-white font-semibold text-xs leading-3   md:text-sm ">
           We work to take care of your health and body{" "}
         </p>
-        <h1 className="text-2xl font-medium md:text-5xl">
+       <h1 className="text-2xl font-extrabold leading-none md:text-5xl">
           True Healthcare
         </h1>
-        <h1 className="text-2xl font-medium md:text-5xl">
+        <h1 className="text-2xl font-extrabold leading-[1px] md:text-5xl">
           For your Family
         </h1>
+       </div>
         {/* <p className="3/4 mx-4 text-center md:text-center md:text-2xl">
           Get up to 18% OFF on Diabetes Care{" "}
         </p> */}
