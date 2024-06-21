8a2c665eab9c8e1825a30608338f210b73c8595a
sofisalmanarif <sofisalman9906@gmail.com>
2024-06-21 22:32:50 +0530

    Changes in hero section

--- Frontend/src/Components/Herosection.jsx
+++ Frontend/src/Components/Herosection.jsx
@@ -1,32 +1,37 @@
-
+import { Link } from "react-router-dom";
+import { FaOpencart } from "react-icons/fa6";
+import { FaShopify } from "react-icons/fa";
 
 function Herosection() {
   return (
-    <div className="slider rounded-3xl md:h-[26rem] w-full flex flex-row md:flex-row text-[#3E5C75] bg-[#B0E5F2] overflow-hidden">
-      <div className="  w-[60%] md:pl-10 left flex flex-col justify-center items-center md:items-baseline px-2 py-4 md:px-12 md:py-12 space-y-2 md:space-y-8 md:ml-[120px]">
+    <div className="slider rounded-3xl md:h-[26rem] h-[15rem] w-full flex flex-row md:flex-row text-[#3E5C75] bg-[#B0E5F2] overflow-hidden">
+      <div className=" w-[50%] md:w-[60%] md:ml-0 ml-1 left flex flex-col justify-center  px-2 py-4  md:py-12 space-y-2 md:space-y-8 md:pl-[9rem]">
          
-       <div>
-       <p className="3/4 mb-2  text-white md:font-semibold text-[8px] md:leading-3   md:text-sm ">
-          We work to take care of your health and body{" "}
-        </p>
-       <h1 className="text-md  leading-5 font-extrabold md:leading-none md:text-[4rem]">
+       <div className="headings md:mt-0 mt-10 z-10  ">
+       
+       <h1 className="text-xl  leading-5 font-extrabold md:leading-none md:text-[3.5rem]">
           True Healthcare
         </h1>
-        <h1 className="text-md  leading-5 font-extrabold md:leading-none md:text-[4rem]">
+        <h1 className="text-xl  leading-5 font-extrabold md:leading-none md:text-[3.5rem]">
           For your Family
         </h1>
+        <p className="  md:w-[70%] mt-1 md:mt-3  text-white md:font-semibold text-[8px] md:leading-4   md:text-sm ">
+          Empowering your family with top-quality health services and products, right at your fingertips
+        </p>
        </div>
         {/* <p className="3/4 mx-4 text-center md:text-center md:text-2xl">
           Get up to 18% OFF on Diabetes Care{" "}
         </p> */}
-        <button className="text-white text-xs bg-[#0071BC] px-4 py-1 my-3 md:px-10 md:py-2 md:my-6 font-bold mx-5 md:text-xl">
-          Shop Now
-        </button>
+        <div className="flex items-center  gap-1  md:gap-5">
+          <Link to="/productspage/medicines" className="text-white flex gap-1 items-center rounded-full w-[5rem] text-[8px] bg-[#0071BC] px-2 py-1  md:px-10 md:py-2 md:my-0   md:mx-0 md:text-[16px] md:w-[12rem]">
+         <FaShopify/> Shop Now
+        </Link>
+        <Link to="/cart" className="p-2 md:p-2 text-white text-[8px] md:text-2xl md:font-extrabold rounded-full mt-[-2] bg-[#0071BC] "> <FaOpencart className="font-extrabold hover:rotate-6"/></Link></div>
       </div>
-      <div className="right w-[40%] md:mr-28 items-baseline justify-center ">
+      <div className="right w-[50%] md:w-[40%] md:mr-28   ">
         <img
-          className="w-full h-full  md:w-[36rem]  md:mx-8"
-          src="pictures/doctornew.png"
+          className="w-full h-full object-fill  md:w-[36rem]  md:mx-8"
+          src="../../pictures/doctor_PNG16021.png"
           alt=""
         />
       </div>
--- Frontend/src/Components/footer/Ccfooter.jsx
+++ Frontend/src/Components/footer/Ccfooter.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+
 import { BsInstagram } from "react-icons/bs";
 import { BsFacebook } from "react-icons/bs";
 import { BsTwitterX } from "react-icons/bs";
@@ -6,7 +6,7 @@ import { BsTwitterX } from "react-icons/bs";
 export default function Ccfooter() {
   return (
     <div className="outer bg-[#EAF7FF] flex flex-col  items-center justify-center mx-0 h-[200px] md:h-[270px] rounded-3xl">
-      <div className="inner bg-white h-[160px] w-[70%] md:h-[225px] md:w-[90%] rounded-3xl pb-10">
+      <div className="inner bg-white h-[165px] w-[95%] md:h-[225px] md:w-[90%] rounded-3xl pb-10">
         <div className="items flex items-center justify-evenly space-x-16 mt-4 mb-6 md:mt-5  md:mb-8">
           <ul className="space-y-1 md:space-y-2">
             <li className="md:pb-3 text-sm md:text-lg font-bold">Company</li>
@@ -14,25 +14,24 @@ export default function Ccfooter() {
             <li className="text-[10px] md:text-sm leading-4">About Us</li>
             <li className="text-[10px] md:text-sm leading-4">Contact Us</li>
           </ul>
+          
 
-          <ul className="hidden sm:block md:visible md:space-y-2">
-            <li className="md:pb-3 text-sm font-bold md:text-lg ">Community</li>
-            <li className="text-[10px] md:text-sm leading-4">
-              CureCart for Business
-            </li>
-            <li className="text-[10px] md:text-sm leading-4">
-              Explore Templates
-            </li>
-            <li className="text-[10px] md:text-sm leading-4">Charities</li>
+          <ul className="space-y-1 md:space-y-2">
+            <li className="md:pb-3 text-sm md:text-lg font-bold">Community</li>
+            <li className="text-[10px] md:text-sm leading-1">CureCart for Business</li>
+            <li className="text-[10px] md:text-sm leading-none">About Us</li>
+            <li className="text-[10px] md:text-sm leading-none">Contact Us</li>
           </ul>
+
+          
 
           <ul className="space-y-1 md:space-y-2">
             <li className="text-sm md:pb-3 font-bold md:text-lg ">Support</li>
-            <li className="text-[10px] md:text-sm leading-4">
+            <li className="text-[10px] md:text-sm leading-1">
               Getting Started
             </li>
-            <li className="text-[10px] md:text-sm leading-4">FAQs</li>
-            <li className="text-[10px] md:text-sm leading-4">
+            <li className="text-[10px] md:text-sm leading-1">FAQs</li>
+            <li className="text-[10px] md:text-sm leading-1">
               Report a Violation
             </li>
           </ul>
@@ -41,7 +40,7 @@ export default function Ccfooter() {
             <li className="md:pb-3 text-sm font-bold md:text-lg ">
               Trust & Legal
             </li>
-            <li className="text-[10px] md:text-sm leading-4">
+            <li className="text-[10px] md:text-sm leading-1">
               Terms and Conditions
             </li>
             <li className="text-[10px] md:text-sm leading-4">Cookie Notice</li>
